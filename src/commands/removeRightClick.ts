80e85f227f7220c68e7d190e1ffd2ffb70b6f0ae
techbot121 <techbot121@gmail.com>
2022-03-26 18:29:20 +0100

    add some error handling to the rightclick command

--- src/commands/removeRightClick.ts
+++ src/commands/removeRightClick.ts
@@ -1,3 +1,4 @@
+import { DiscordAbortCodes } from 'detritus-client-rest/lib/constants';
 import { ApplicationCommandType, CommandContext, SlashCommand, SlashCreator } from 'slash-create';
 import { discord, EphemeralResponse, hasPermissions, Permissions } from '../util';
 
@@ -14,8 +15,20 @@ export default class RemoveRightClickCommand extends SlashCommand {
     const allowed =
       hasPermissions(ctx, Permissions.MANAGE_MESSAGES) || ctx.user.id === (message.mentions as object)[0]?.id; // object until upstream is fixed
     if (allowed) {
-      await discord.deleteMessage(CHANNEL, message.id);
-      return EphemeralResponse("Ok, I've removed that submission.");
+      try {
+        await discord.deleteMessage(CHANNEL, message.id);
+        return EphemeralResponse("Ok, I've removed that submission.");
+      } catch (ex) {
+        const code = ex as DiscordAbortCodes;
+        switch (code) {
+          case DiscordAbortCodes.UNKNOWN_MESSAGE:
+            return EphemeralResponse('This message is not from the Submission Channel.');
+          default: {
+            console.error(ex);
+            return EphemeralResponse('Something went wrong while trying to remove the Message, try again later...');
+          }
+        }
+      }
     } else {
       return EphemeralResponse("Sorry, that's not your submission or you don't have enough Permission to remove it.");
     }
